192d82db224f5e936159594226db96d4a6f5160c
Alexander Schramm <alexander.schramm1992@gmail.com>
2022-11-22 22:52:46 +0100

    Erstelle Login Panel

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,12 @@ import Browser
 import Html exposing (Html, text)
 import Html exposing (div)
 import Html.Attributes exposing (class)
+import Html exposing (input)
+import Html.Attributes exposing (type_)
+import Html.Attributes exposing (placeholder)
+import Html exposing (br)
+import Html.Attributes exposing (value)
+import Html.Attributes exposing (align)
 
 type Model = Model
 
@@ -28,10 +34,31 @@ view model =
   { title = "Space Realm"
   , body = [
     div 
-      [class "background"] 
-      [div 
-        [class "panel"] 
-        [ text "Hello Space!"] ] ] }
+      [ class "background" ] 
+      [ div 
+        [ class "panel glow" ] 
+        [ div
+          [ class "center" ]
+          [ text "Space Realm" ]
+        , login model ] ] ] }
+
+login: Model -> Html Msg
+login model = div
+  [ class "panel" ]
+  [ input
+    [ type_ "text"
+    , placeholder "Benutzername" ]
+    []
+  , br [] []
+  , input
+    [ type_ "password" 
+    , placeholder "Passwort" ]
+    []
+  , br [] []
+  , input
+    [ type_ "button"
+    , value "Login" ]
+    [ ] ]
 
 update: Msg -> Model -> (Model, Cmd Msg)
 update msg model =
